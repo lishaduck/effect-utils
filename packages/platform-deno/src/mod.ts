2d5f5c887b30f9150da4ba56fabe56474baf7d65
Eli <88557639+lishaduck@users.noreply.github.com>
2024-11-13 15:27:06 -0600

    docs: document

--- packages/platform-deno/src/mod.ts
+++ packages/platform-deno/src/mod.ts
@@ -1,2 +1,33 @@
+/**
+ * @file
+ *
+ * ```ts
+ * import { Path } from "@effect/platform";
+ * import { DenoContext, DenoRuntime } from "@lishaduck/effect-platform-deno";
+ * import { Console, Effect } from "effect";
+ *
+ * const program = Effect.gen(function* () {
+ *   // Access the Path service
+ *   const path = yield* Path.Path;
+ *
+ *   // Join parts of a path to create a complete file path
+ *   const tmpPath = path.join("tmp", "file.txt");
+ *
+ *   yield* Console.log(tmpPath);
+ * });
+ *
+ * DenoRuntime.runMain(program.pipe(Effect.provide(DenoContext.layer)));
+ * ```
+ */
+
+/**
+ * @file
+ * Provides a platform context using Deno.
+ */
 export * as DenoContext from "./DenoContext.ts";
+
+/**
+ * @file
+ * An effect runtime using Deno.
+ */
 export * as DenoRuntime from "./DenoRuntime.ts";
