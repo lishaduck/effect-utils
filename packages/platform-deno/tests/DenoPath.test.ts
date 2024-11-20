83c20a4a26d949f25ff0be689d3ac5d03c79cbf6
Eli <88557639+lishaduck@users.noreply.github.com>
2024-11-20 08:35:24 -0600

    refactor: use `it.effect`

--- packages/platform-deno/tests/DenoPath.test.ts
+++ packages/platform-deno/tests/DenoPath.test.ts
@@ -1,23 +1,18 @@
 import { Path } from "@effect/platform";
-import { describe, it } from "@effect/vitest";
+import { it } from "@effect/vitest";
 import { Effect } from "effect";
 import * as DenoPath from "../src/DenoPath.ts";
 
-const runPromise = <E, A>(self: Effect.Effect<A, E, Path.Path>): Promise<A> =>
-  Effect.runPromise(Effect.provide(self, DenoPath.layer));
+it.layer(DenoPath.layer)("Integration", (it) => {
+  it.effect("Runs demo program.", ({ expect }) =>
+    Effect.gen(function* () {
+      // Access the Path service
+      const path = yield* Path.Path;
 
-const program = Effect.gen(function* () {
-  // Access the Path service
-  const path = yield* Path.Path;
+      // Join parts of a path to create a complete file path
+      const tmpPath = path.join("tmp", "file.txt");
 
-  // Join parts of a path to create a complete file path
-  return path.join("tmp", "file.txt");
-});
-
-describe.concurrent("Integration", () => {
-  it("Runs demo program.", async ({ expect }) => {
-    const output = await runPromise(program);
-
-    expect(output).toBe("tmp/file.txt");
-  });
+      expect(tmpPath).toBe("tmp/file.txt");
+    }),
+  );
 });
